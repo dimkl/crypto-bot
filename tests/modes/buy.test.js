be7096f982c56f7b5b20ee4b49db024ab1f1cd31
Dimitris Klouvas <klouvas@workable.com>
2021-02-02 23:47:14 +0200

    add: run tests with jest

--- tests/modes/buy.test.js
+++ tests/modes/buy.test.js
@@ -1,5 +1,5 @@
-const DB = require('../src/db');
-const buyMode = require('../src/modes/buy');
+const DB = require('../../src/db');
+const buyMode = require('../../src/modes/buy');
 const config = {
   buyMode: {
     changePercentage: '0.1000',
@@ -9,38 +9,37 @@ const config = {
   currencyPair: 'xlmeur'
 };
 
-function test(testCase, cb) {
-  Promise.resolve(cb())
-    .then((condition) => console.assert(condition, testCase))
-    .catch(console.error);
-}
+describe("buy mode", () => {
+  beforeAll(() => {
+    console.log = jest.fn();
+  });
 
-test("buy: when value dropping, updates buying state with min value until comeback decrease from latest value", async () => {
-  DB[config.currencyPair] = DB[config.currencyPair] || {};
-  Object.assign(DB[config.currencyPair], { capital: 50, assets: 0 });
-  Object.assign(DB[config.currencyPair].state, { buying: null, bought: null });
+  test("buy: when value dropping, updates buying state with min value until comeback decrease from latest value", async () => {
+    DB[config.currencyPair] = DB[config.currencyPair] || {};
+    Object.assign(DB[config.currencyPair], { capital: 50, assets: 0 });
+    Object.assign(DB[config.currencyPair].state, { buying: null, bought: null });
 
-  const data = [
-    { currentBid: 114, hourlyOpen: 120, hourlyBid: 120 },
-    { currentBid: 106, hourlyOpen: 120, hourlyBid: 120 },
-    { currentBid: 102, hourlyOpen: 120, hourlyBid: 120 },
-    { currentBid: 98, hourlyOpen: 120, hourlyBid: 120 },
-    { currentBid: 99, hourlyOpen: 120, hourlyBid: 120 },
-    { currentBid: 100, hourlyOpen: 120, hourlyBid: 120 },
-    { currentBid: 95, hourlyOpen: 120, hourlyBid: 120 },
-    { currentBid: 94, hourlyOpen: 120, hourlyBid: 120 }
-  ];
+    const data = [
+      { currentBid: 114, hourlyOpen: 120, hourlyBid: 120 },
+      { currentBid: 106, hourlyOpen: 120, hourlyBid: 120 },
+      { currentBid: 102, hourlyOpen: 120, hourlyBid: 120 },
+      { currentBid: 98, hourlyOpen: 120, hourlyBid: 120 },
+      { currentBid: 99, hourlyOpen: 120, hourlyBid: 120 },
+      { currentBid: 100, hourlyOpen: 120, hourlyBid: 120 },
+      { currentBid: 95, hourlyOpen: 120, hourlyBid: 120 },
+      { currentBid: 94, hourlyOpen: 120, hourlyBid: 120 }
+    ];
 
-  for (const dt of data) {
-    Object.assign(DB[config.currencyPair], dt);
-    await buyMode(config);
-    
-    // end buying
-    if(DB[config.currencyPair].state.bought) DB[config.currencyPair].capital = 0;
-  }
+    for (const dt of data) {
+      Object.assign(DB[config.currencyPair], dt);
+      await buyMode(config);
 
-  const buyingExpectation = DB[config.currencyPair].state.buying === null;
-  const boughtExpectation = DB[config.currencyPair].state.bought === 100;
+      // end buying
+      if (DB[config.currencyPair].state.bought) DB[config.currencyPair].capital = 0;
+    }
 
-  return buyingExpectation && boughtExpectation;
+    expect(DB[config.currencyPair].state.buying).toBeNull();
+    expect(DB[config.currencyPair].state.bought).toBe(100);
+  });
 });
+
--- tests/modes/sell.test.js
+++ tests/modes/sell.test.js
@@ -1,7 +1,7 @@
-const DB = require('../src/db');
-const sellMode = require('../src/modes/sell');
+const DB = require('../../src/db');
+const sellMode = require('../../src/modes/sell');
 const config = {
-  sellMode:{
+  sellMode: {
     changePercentage: '0.1000',
     comebackPercentage: '0.0200',
     tradePercentage: '1.0000'
@@ -9,38 +9,36 @@ const config = {
   currencyPair: 'xlmeur'
 };
 
-function test(testCase, cb) {
-  Promise.resolve(cb())
-    .then((condition) => console.assert(condition, testCase))
-    .catch(console.error);
-}
+describe("sell mode", () => {
+  beforeAll(() => {
+    console.log = jest.fn();
+  });
 
-test("sell: when value rising, updates selling state with max value until comeback decrease from latest value", async () => {
-  DB[config.currencyPair] = DB[config.currencyPair] || {};
-  Object.assign(DB[config.currencyPair], { capital:0, assets: 50 });
-  Object.assign(DB[config.currencyPair].state, { selling: null, bought: 100 });
+  test("sell: when value rising, updates selling state with max value until comeback decrease from latest value", async () => {
+    DB[config.currencyPair] = DB[config.currencyPair] || {};
+    Object.assign(DB[config.currencyPair], { capital: 0, assets: 50 });
+    Object.assign(DB[config.currencyPair].state, { selling: null, bought: 100 });
 
-  const data = [
-    { currentAsk: 98, hourlyAsk: 100 },
-    { currentAsk: 108, hourlyAsk: 100 },
-    { currentAsk: 110, hourlyAsk: 100 },
-    { currentAsk: 114, hourlyAsk: 100 },
-    { currentAsk: 118, hourlyAsk: 100 },
-    { currentAsk: 109, hourlyAsk: 100 },
-    { currentAsk: 120, hourlyAsk: 100 },
-    { currentAsk: 95, hourlyAsk: 100 },
-  ];
+    const data = [
+      { currentAsk: 98, hourlyAsk: 100 },
+      { currentAsk: 108, hourlyAsk: 100 },
+      { currentAsk: 110, hourlyAsk: 100 },
+      { currentAsk: 114, hourlyAsk: 100 },
+      { currentAsk: 118, hourlyAsk: 100 },
+      { currentAsk: 109, hourlyAsk: 100 },
+      { currentAsk: 120, hourlyAsk: 100 },
+      { currentAsk: 95, hourlyAsk: 100 },
+    ];
 
-  for (const dt of data) {
-    Object.assign(DB[config.currencyPair], dt);
-    await sellMode(config);
-    
-    // end selling
-    if(DB[config.currencyPair].state.sold) DB[config.currencyPair].assets = 0;
-  }
+    for (const dt of data) {
+      Object.assign(DB[config.currencyPair], dt);
+      await sellMode(config);
 
-  const sellingExpectation = DB[config.currencyPair].state.selling === null;
-  const soldExpectation = DB[config.currencyPair].state.sold === 109;
-  
-  return sellingExpectation && soldExpectation;
+      // end selling
+      if (DB[config.currencyPair].state.sold) DB[config.currencyPair].assets = 0;
+    }
+
+    expect(DB[config.currencyPair].state.selling).toBeNull();
+    expect(DB[config.currencyPair].state.sold).toBe(109);
+  });
 });
