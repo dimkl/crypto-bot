c198c51e0766daec2255e5652340d80b199d8dc4
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2021-02-02 23:47:14 +0200

    add: run tests with jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  testEnvironment: "node",
+};
---
+++ tests/index.test.js
@@ -0,0 +1,11 @@
+describe("e2e", () => {
+  test("executes setup mode", async () => {
+    expect(true).toBe(true);
+  });
+  test("executes buy mode", async () => {
+    expect(true).toBe(true);
+  });
+  test("executes sell mode", async () => {
+    expect(true).toBe(true);
+  });
+});
