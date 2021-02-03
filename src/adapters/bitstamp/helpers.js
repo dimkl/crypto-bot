323caa00a86edf38ef7b352aebfbb526bc476560
Dimitris Klouvas <klouvas@workable.com>
2021-02-04 01:25:57 +0200

    fix: getUserTransactions api

--- src/adapters/bitstamp/helpers.js
+++ src/adapters/bitstamp/helpers.js
@@ -1,24 +1,33 @@
+function splitCurrencies(currencyPair) {
+    return [currencyPair.slice(0, 3), currencyPair.slice(3)];
+}
 
 function getAvailableKeys(currencyPair) {
-    return [currencyPair.slice(0, 3), currencyPair.slice(3)].map(s => `${s}_available`);
+    return splitCurrencies(currencyPair).map(s => `${s}_available`);
 }
 
 function getFeeKey(currencyPair) {
     return `${currencyPair}_fee`;
 }
 
-function getTransactionType(type){
-    const mapping = { 0: 'deposit', 1: 'withdrawl', 2: 'market_trade', 14: 'sub_account_transfer'};
+function getTransactionType(type) {
+    const mapping = { 0: 'deposit', 1: 'withdrawl', 2: 'market_trade', 14: 'sub_account_transfer' };
     return mapping[type];
 }
 
-function getExchangeRateKey(currencyPair){
-    return getAvailableKeys(currencyPair).join('_');
+function getExchangeRateKey(currencyPair) {
+    return splitCurrencies(currencyPair).join('_');
 }
 
-module.exports = { 
+function getExchangeType(capital) {
+    return capital < 0 ? 'sell' : 'buy';
+}
+
+module.exports = {
+    splitCurrencies,
     getFeeKey,
     getAvailableKeys,
     getTransactionType,
-    getExchangeRateKey
+    getExchangeRateKey,
+    getExchangeType
 };
