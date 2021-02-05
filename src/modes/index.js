1f0c58acdab7087754951d0b9d831b35e7c2dd0f
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2021-02-05 02:21:23 +0200

    cleanup: remove snapshotdb mode

--- src/modes/index.js
+++ src/modes/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   setupMode: require('./setup'),
   buyMode: require('./buy'),
-  sellMode: require('./sell'),
-  snapshotDBMode: require('./snapshotDB')
+  sellMode: require('./sell')
 };
