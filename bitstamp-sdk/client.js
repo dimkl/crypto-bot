ee1a8b76f07a8159ec0eb6f06e7cf09bedf2a00d
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2021-02-06 18:05:04 +0200

    add: 1 retry on GET requests that timeout

--- bitstamp-sdk/client.js
+++ bitstamp-sdk/client.js
@@ -1,3 +1,10 @@
 const got = require('got');
 
-module.exports = got.extend({ timeout: 4000 });
+module.exports = got.extend({
+  timeout: 4000,
+  retry: {
+    limit: 1,
+    methods: ['GET'],
+    errorCodes: ['ETIMEDOUT']
+  }
+});
