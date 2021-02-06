e50a322f9c95ba7544fca6fec44b85410d459625
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2021-02-07 00:14:45 +0200

    change xlmeur,xrpeur buy|sell with 100%

--- src/models/config.js
+++ src/models/config.js
@@ -7,14 +7,14 @@ const Config = db.defaults({
     {
       currencyPair: 'xlmeur',
       interval: intervalSeconds(5),
-      buyMode: makeModeConfig(50, 2, 0.5),
-      sellMode: makeModeConfig(50, 2, 0.5),
+      buyMode: makeModeConfig(100, 2, 0.5),
+      sellMode: makeModeConfig(100, 2, 0.5),
     },
     {
       currencyPair: 'xrpeur',
       interval: intervalSeconds(5),
-      buyMode: makeModeConfig(50, 2, 0.5),
-      sellMode: makeModeConfig(50, 2, 0.5),
+      buyMode: makeModeConfig(100, 2, 0.5),
+      sellMode: makeModeConfig(100, 2, 0.5),
     },
     {
       currencyPair: 'btceur',
