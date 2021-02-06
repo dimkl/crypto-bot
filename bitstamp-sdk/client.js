b74468b3fb940b318245f675fe6b6ea142e9be2c
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2021-02-06 19:12:31 +0200

    add: dns lookup cache

--- bitstamp-sdk/client.js
+++ bitstamp-sdk/client.js
@@ -1,6 +1,10 @@
 const got = require('got');
+const CacheableLookup = require('cacheable-lookup');
+
+const dnsCache = new CacheableLookup();
 
 module.exports = got.extend({
+  dnsCache,
   timeout: 4000,
   retry: {
     limit: 1,
