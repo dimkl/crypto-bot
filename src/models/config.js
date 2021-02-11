3b26bd067c46413325095a025888a197b3101692
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2021-02-12 01:11:36 +0200

    add: btceur currency pair & update config percentages

--- src/models/config.js
+++ src/models/config.js
@@ -7,26 +7,26 @@ const Config = db.defaults({
     {
       currencyPair: 'xlmeur',
       interval: intervalSeconds(5),
-      buyMode: makeModeConfig(100, 4, 1),
-      sellMode: makeModeConfig(100, 2.5, 1),
+      buyMode: makeModeConfig(100, 5, 1.5),
+      sellMode: makeModeConfig(100, 5, 1.5),
     },
     {
       currencyPair: 'xrpeur',
       interval: intervalSeconds(5),
-      buyMode: makeModeConfig(100, 4, 1),
-      sellMode: makeModeConfig(100, 2.5, 1),
+      buyMode: makeModeConfig(100, 5, 1.5),
+      sellMode: makeModeConfig(100, 5, 1.5),
     },
     {
       currencyPair: 'btceur',
       interval: intervalSeconds(5),
-      buyMode: makeModeConfig(50, 4, 1),
-      sellMode: makeModeConfig(50, 3, 1),
+      buyMode: makeModeConfig(100, 5, 1),
+      sellMode: makeModeConfig(100, 8, 2),
     },
     {
       currencyPair: 'omgeur',
       interval: intervalSeconds(5),
-      buyMode: makeModeConfig(100, 6, 1),
-      sellMode: makeModeConfig(100, 8, 2),
+      buyMode: makeModeConfig(100, 5, 1),
+      sellMode: makeModeConfig(100, 5, 1),
     }
   ]
 }).get('config');
