99744aafe6ad0109bd69fa7e8b226040f45b23f8
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2021-02-12 01:41:15 +0200

    fix: decrease the sell & buy change %

--- src/models/config.js
+++ src/models/config.js
@@ -7,14 +7,14 @@ const Config = db.defaults({
     {
       currencyPair: 'xlmeur',
       interval: intervalSeconds(5),
-      buyMode: makeModeConfig(100, 5, 1.5),
-      sellMode: makeModeConfig(100, 5, 1.5),
+      buyMode: makeModeConfig(100, 4, 1),
+      sellMode: makeModeConfig(100, 4, 1),
     },
     {
       currencyPair: 'xrpeur',
       interval: intervalSeconds(5),
-      buyMode: makeModeConfig(100, 5, 1.5),
-      sellMode: makeModeConfig(100, 5, 1.5),
+      buyMode: makeModeConfig(100, 4, 1),
+      sellMode: makeModeConfig(100, 4, 1),
     },
     {
       currencyPair: 'btceur',
