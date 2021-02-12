e4f3a4ed679b97c33be1ff99e4402d5c82e4b384
Dimitris Klouvas <klouvas@workable.com>
2021-02-13 00:55:00 +0200

    refactor: bitstamp-sdk apiKey & apiSecret and throw required error is not set

--- bitstamp-sdk/authorization.js
+++ bitstamp-sdk/authorization.js
@@ -4,12 +4,13 @@ const { URL } = require('url');
 const { v4: uuidv4 } = require('uuid');
 const client = require('./client');
 
-const API_KEY = process.env.BITSTAMP_API_KEY;
-const API_SECRET = process.env.BITSTAMP_API_SECRET;
+function _concatObjectKeys(obj, keys) {
+    return keys.reduce((acc, key) => acc + obj[key], '');
+}
 
-function _getAuthHeaders(includeContent) {
+function _getAuthHeaders(includeContent, apiKey) {
     return {
-        'x-auth': `BITSTAMP ${API_KEY}`,
+        'x-auth': `BITSTAMP ${apiKey}`,
         'x-auth-nonce': uuidv4(),
         'x-auth-timestamp': Date.now(),
         'x-auth-version': 'v2',
@@ -19,13 +20,10 @@ function _getAuthHeaders(includeContent) {
 
 function _getSignatureBody(httpVerb, url, headers, bodyStr = '') {
     const { host, pathname, query = "" } = new URL(url);
-
     const urlStr = httpVerb.toUpperCase() + host + pathname + query;
-    let headerStr = [
-        'x-auth-nonce',
-        'x-auth-timestamp',
-        'x-auth-version'
-    ].reduce((acc, key) => acc + headers[key], '');
+
+    const headerKeys = ['x-auth-nonce', 'x-auth-timestamp', 'x-auth-version'];
+    let headerStr = _concatObjectKeys(headers, headerKeys);
 
     if (bodyStr) {
         headerStr = headers['content-type'] + headerStr;
@@ -34,24 +32,25 @@ function _getSignatureBody(httpVerb, url, headers, bodyStr = '') {
     return headers['x-auth'] + urlStr + headerStr + bodyStr;
 };
 
-function _createSignature(content, apiSecret = API_SECRET) {
+function _createSignature(content, apiSecret) {
     const hmac = crypto.createHmac('sha256', apiSecret);
     return hmac.update(content).digest('hex');
 }
 
 function _verifyResponseSignature(headers, responseHeaders, responseContent, apiSecret) {
-    const nonce = headers['x-auth-nonce'];
-    const timestamp = headers['x-auth-timestamp'];
+    const headerStr = _concatObjectKeys(headers, ['x-auth-nonce', 'x-auth-timestamp']);
     const contentType = responseHeaders['content-type'];
 
-    const signatureContent = nonce + timestamp + contentType + responseContent;
+    const signatureContent = headerStr + contentType + responseContent;
     const signature = _createSignature(signatureContent, apiSecret);
     const responseSignature = responseHeaders['x-server-auth-signature'];
 
     assert(signature === responseSignature, 'Signatures do not match');
 }
 
-async function authorizedRequest(url, method, body, { apiKey, apiSecret } = {}) {
+async function authorizedRequest(url, method, body, authConfig) {
+    const { apiKey, apiSecret } = authConfig;
+
     const headers = _getAuthHeaders(!!body, apiKey);
     const signatureContent = _getSignatureBody(method, url, headers, body);
     headers['x-auth-signature'] = _createSignature(signatureContent, apiSecret);
--- bitstamp-sdk/index.js
+++ bitstamp-sdk/index.js
@@ -12,6 +12,9 @@ const ENDPOINTS = Object.freeze([
   { path: 'buy/{currencyPair}/', method: 'POST', authorized: true, defaultResponse: {} },
   { path: 'user_transactions/', method: 'POST', authorized: true, defaultResponse: [] }
 ]);
+const API_KEY = process.env.BITSTAMP_API_KEY;
+const API_SECRET = process.env.BITSTAMP_API_SECRET;
+const REQUIRED_CONFIGURATION = Object.freeze(['apiKey', 'apiSecret']);
 
 function replaceAndRemovePathParams(path, params) {
   return path.replace(/{(.*)}/, (_, $1) => {
@@ -49,6 +52,12 @@ function createAuthorizedApiMethod({ path, method }, { apiKey, apiSecret } = {})
 }
 
 module.exports = function Api(options = {}) {
+  options = { apiKey: API_KEY, apiSecret: API_SECRET, ...options };
+
+  if (!REQUIRED_CONFIGURATION.every(k => options[k])) {
+    throw new Error('apiKey and apiSecret are required. Provide them explicit or via env variable');
+  }
+
   return ENDPOINTS.reduce((api, apiConfig) => {
     const factory = apiConfig.authorized ? createAuthorizedApiMethod : createApiMethod;
     const name = camelCase(apiConfig.path.split('/').shift());
