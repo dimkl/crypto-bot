9bed9ff3c05cee7b52b56e289b937c7775a12109
Dimitris Klouvas <klouvas@workable.com>
2021-02-13 04:11:14 +0200

    fix: reduce interval to 3 seconds

--- src/models/config.js
+++ src/models/config.js
@@ -6,21 +6,21 @@ const Config = db.defaults({
   config: [
     {
       currencyPair: 'xlmeur',
-      interval: intervalSeconds(5),
+      interval: intervalSeconds(3),
       buyMode: makeModeConfig(100, 5, 1.5),
       sellMode: makeModeConfig(100, 4, 1),
       auth: { apiSecret: '', apiKey: '' }
     },
     {
       currencyPair: 'xrpeur',
-      interval: intervalSeconds(5),
+      interval: intervalSeconds(3),
       buyMode: makeModeConfig(100, 5, 1.5),
       sellMode: makeModeConfig(100, 4, 1.5),
       auth: { apiSecret: '', apiKey: '' }
     },
     {
       currencyPair: 'btceur',
-      interval: intervalSeconds(5),
+      interval: intervalSeconds(3),
       buyMode: makeModeConfig(100, 5, 1),
       sellMode: makeModeConfig(100, 8, 2),
       auth: { apiSecret: '', apiKey: '' }
